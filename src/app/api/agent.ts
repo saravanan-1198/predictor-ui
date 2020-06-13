2fe7d8ed281adff1bc7241708ab7fdbc18da89d1
dhiliph98 <dhiliph98@outlook.com>
2020-06-13 17:42:37 +0530

    Pointing to GAE

--- src/app/api/agent.ts
+++ src/app/api/agent.ts
@@ -3,8 +3,8 @@ import { history } from "../../index";
 import { IPredictionInput } from "../models/prediction-input.model";
 import { message } from "antd";
 
-axios.defaults.baseURL = "http://localhost:5000/api";
-// axios.defaults.baseURL = "https://salesprediction.el.r.appspot.com/api";
+// axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = "https://salesprediction.el.r.appspot.com/api";
 
 axios.interceptors.response.use(undefined, (error) => {
   if (error.response.status === 404) {
