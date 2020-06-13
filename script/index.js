55c83fe39dd7aa28fda91a249fda2c40a93913bd
dhiliph98 <dhiliph98@outlook.com>
2020-06-13 17:48:18 +0530

    bug fix

--- script/index.js
+++ script/index.js
@@ -4,6 +4,7 @@ var fs = require("fs");
 exec(
   'gcloud secrets versions access latest --secret="Firebase_Auth_JSON"',
   (err, out) => {
+    console.log(out);
     fs.writeFileSync(
       ".env",
       `REACT_APP_FIREBASE_AUTH_JSON=${JSON.stringify(JSON.parse(out))}`
