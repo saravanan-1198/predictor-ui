32fd2546ea9e0d9a6d83d40e56292ee8527e18d2
Aswin <ftworgtech@gmail.com>
2020-07-23 17:51:34 +0530

    gcp decoupling

--- script/index.js
+++ script/index.js
@@ -1,9 +1,18 @@
 var exec = require("child_process").exec;
 var fs = require("fs");
-
-const string = fs.readFileSync("test.json", "utf-8");
-
-fs.writeFileSync(
+var axios = require("axios");
+var key = "s3cr3tEncrypt1onKey";
+var decryptor = require("simple-encryptor")(key);
+axios.get("http://localhost:5000/api/secret",{
+  headers: {
+    "secret": "bypassKeyForSecret"
+  }
+}).then((res)=>{
+  var obj = decryptor.decrypt(res.data);
+  // console.log(obj);
+  fs.writeFileSync(
   ".env",
-  `REACT_APP_FIREBASE_AUTH_JSON=${JSON.stringify(JSON.parse(string))}`
+  `REACT_APP_FIREBASE_AUTH_JSON=${JSON.stringify(obj)}`
 );
+})
+// const string = fs.readFileSync("test.json", "utf-8");
